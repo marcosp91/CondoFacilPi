76f1c62379f59a5e6e56c458a48481880058e7b5
Marcos Paulo <condofacilpi@gmail.com>
2017-11-19 21:56:25 -0300

    Incluindo Landing Page/Google Analitycs

---
+++ app/Http/Controllers/ContatosController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Painel\Contato;
+use Illuminate\Support\Facades\DB;
+use function redirect;
+use function view;
+
+
+class ContatosController extends Controller
+{
+    private $contato;
+
+	public function __construct(Contato $contato)
+	{
+		$this->contato = $contato;
+	}
+
+	public function insereContato(Request $request)
+	{
+		$dadosForm = $request->all();
+		$novoContato = $dadosForm;
+
+		$insert = $this->contato->create($novoContato);
+		if($insert){
+
+			$pathToFile = 'storage/ebook.pdf';
+			return response()->download($pathToFile);
+
+			return redirect()->route('index.landing');
+		}
+
+	}
+}
---
+++ resources/views/template/landingPage.blade.php
@@ -0,0 +1,27 @@
+<?php if (!isset($_SESSION)) {
+    session_start();
+} ?>
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Condo Fácilasdsa - Landing Page</title>
+        
+        <!-- Bootstrap & CSS Bibliotecas -->
+        <link href="/css/estilo.css" rel="stylesheet">
+        
+        <!-- JQuery Bibliotecas -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="/js/jquery-3.2.1.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+    </head>
+   
+    
+    <body>
+        <img src="/img/logos/logo01.png" class="img-reponsive">
+        asdada
+    </body>
+   
+</html>
