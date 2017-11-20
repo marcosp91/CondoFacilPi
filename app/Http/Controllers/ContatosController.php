e8da4f1dd351124855f474eab0a94ee4886ff68d
Marcos Paulo <condofacilpi@gmail.com>
2017-11-20 20:59:22 -0300

    teste ebook

--- app/Http/Controllers/ContatosController.php
+++ app/Http/Controllers/ContatosController.php
@@ -26,7 +26,7 @@ class ContatosController extends Controller
 		$insert = $this->contato->create($novoContato);
 		if($insert){
 
-			$pathToFile = 'storage/public/ebook.pdf';
+			$pathToFile = 'storage/ebook.pdf';
 			return response()->download($pathToFile);
 
 			//return redirect()->route('index.landing');
